4a1c03611ac7eb7da55cb605e48eb06967d8eb42
ovpn-dev <ovpn05@gmail.com>
2023-08-10 10:52:48 +0100

    updated tawk.to api

--- app/page.js
+++ app/page.js
@@ -11,6 +11,21 @@ import Nav from "../components/Nav";
 import VideoBg from "../components/Videobg";
 
 export default function Home() {
+  useEffect(() => {
+    const showText = () => {
+      var Tawk_API = Tawk_API || {};
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/64d3606ccc26a871b02e330d/1h7cr1c2k";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    };
+
+    showText();
+  }, []);
+
   // var Tawk_API = Tawk_API || {},
   //   Tawk_LoadStart = new Date();
   // (function () {
