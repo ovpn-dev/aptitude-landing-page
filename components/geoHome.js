bf998fcb3c3b307ea8505e221a78f5e668a6e9f1
ovpn-dev <ovpn05@gmail.com>
2023-08-11 01:26:00 +0100

    recentered search button

--- components/geoHome.js
+++ components/geoHome.js
@@ -92,7 +92,16 @@ export default function GeoHome() {
       </h3>
       <form onSubmit={handleSubmit}>
         <div class="relative">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <input
+            type="search"
+            id="address"
+            value={userLocation}
+            onChange={(e) => setUserLocation(e.target.value)}
+            class="block w-full p-4 pl-10 text-xs text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter your address here..."
+            required
+          />{" "}
+          <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
             <svg
               class="w-4 h-4 text-gray-500 dark:text-gray-400"
               aria-hidden="true"
@@ -109,20 +118,14 @@ export default function GeoHome() {
               />
             </svg>
           </div>
-          <input
-            type="search"
-            id="address"
-            value={userLocation}
-            onChange={(e) => setUserLocation(e.target.value)}
-            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Enter your address here..."
-            required
-          />
+        </div>{" "}
+        <div className="mt-5">
+          {" "}
           <button
             type="submit"
-            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-xs px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Search nearby homes
+            Search for completed jobs near you
           </button>
         </div>
       </form>
